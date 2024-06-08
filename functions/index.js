d92a9ae6de08f237cb253f5189c9669784d9f59e
br.carvalho <br.carvalho@campus.fct.unl.pt>
2024-06-08 19:12:19 +0100

    Updated main.dart

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,51 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./admin-key.json");
 
-exports.notifyOnThreshold = functions.database.ref('/Terrariums/{terrariumId}/activity/{date}/{hour}')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://scmu-terrariums-default-rtdb.europe-west1.firebasedatabase.app/",
+});
+
+exports.notifyOnThreshold = functions.database
+    .ref("/Terrariums/-NxOimqv_QLBmqdP6ToG/temperature")
     .onUpdate((change, context) => {
-        const newValue = change.after.val();
-        const previousValue = change.before.val();
+      const newValue = change.after.val();
+      const previousValue = change.before.val();
 
-        const threshold = 30;
+      console.log("Trigger function executed");
+      console.log("Previous Value:", previousValue);
+      console.log("New Value:", newValue);
+      console.log("New Value.temperature:", newValue.temperature);
 
-        if (newValue.temperature > threshold && previousValue.temperature <= threshold) {
-            const payload = {
-                notification: {
-                    title: 'Alerta de Limite',
-                    body: `O valor de temperature ultrapassou ${threshold}`,
-                    sound: 'default',
-                }
-            };
+      if (newValue && previousValue) {
+        console.log("Both previous and new values are valid.");
+        if (
+          newValue > 30 ||
+          newValue <= 0
+        ) {
+          const message = {
+            data: {
+              title: "Alerta de Limite",
+              body: "O valor de temperature ultrapassou o limite!",
+            },
+            topic: "temperature",
+          };
 
-            // Envie a notificação para o tópico "temperature"
-            return admin.messaging().sendToTopic('temperature', payload)
-                .then(response => {
-                    console.log('Notificação enviada com sucesso:', response);
-                    return null;
-                })
-                .catch(error => {
-                    console.log('Erro ao enviar notificação:', error);
-                    return null;
-                });
+          return admin.messaging().send(message)
+              .then((response) => {
+                console.log("Notificação enviada com sucesso:", response);
+                return null;
+              })
+              .catch((error) => {
+                console.log("Erro ao enviar notificação:", error);
+                return null;
+              });
         } else {
-            return null;
+          return null;
         }
+      } else {
+        console.log("Invalid temperature values.");
+        return null;
+      }
     });
